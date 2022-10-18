74ee8a51585fa107f72e40f98d3799c7ecb5b7eb
AskarKadir <askarkadir07@gmail.com>
2022-10-18 20:03:45 +0700

    import start.spring.oi

---
+++ src/main/java/askarpws/askarkadir101/Askarkadir101Application.java
@@ -0,0 +1,13 @@
+package askarpws.askarkadir101;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Askarkadir101Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Askarkadir101Application.class, args);
+	}
+
+}
---
+++ src/test/java/askarpws/askarkadir101/Askarkadir101ApplicationTests.java
@@ -0,0 +1,13 @@
+package askarpws.askarkadir101;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Askarkadir101ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
